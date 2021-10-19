f5a63f28411b6e4c96b01ab98be1a1590a970ce4
mafulong <mafulong@bytedance.com>
2021-10-19 20:03:40 +0800

    init

---
+++ json_test.go
@@ -0,0 +1,8 @@
+package go_utils
+
+import "testing"
+
+func TestToJSON(t *testing.T) {
+	t.Log(ToJSON("{}", true))
+	t.Log(ToJSON("{}"))
+}
---
+++ shell.go
@@ -0,0 +1,23 @@
+package go_utils
+
+import (
+	"fmt"
+	"github.com/ionrock/procs"
+)
+
+//CallCommand
+func CallCommand(cmd string) error {
+	p := procs.NewProcess(cmd)
+	p.OutputHandler = func(line string) string {
+		return line
+	}
+	p.ErrHandler = func(line string) string {
+		return line
+	}
+	return p.Run()
+}
+
+//go fmt
+func GoFmt(file string) {
+	CallCommand(fmt.Sprintf("go fmt %q", file))
+}
